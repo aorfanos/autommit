a84e021d29e59223976ed1166bda073e1d1d71e5
Alex Orfanos <aorfanos@users.noreply.github.com>
2023-04-23 01:51:31 +0300

    feat(utils): Add OpenAI client and completion request functions
    Added a new file utils/openai.go with functions NewOpenAiClient and CreateCompletionRequest to create a new OpenAI client and create a chat completion request with a provided prompt respectively. This will allow for easier integration with OpenAI's GPT-20 language model.
    
    Created by autommit 🦄

---
+++ utils/openai.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+
+	// "github.com/sashabaranov/go-openai"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func (a *Autommit) NewOpenAiClient() (*openai.Client) {
+	return openai.NewClient(a.OpenAiApiKey)
+}
+
+func (a *Autommit) CreateCompletionRequest(prompt string) (string, error) {
+
+	var buildResp []string
+	var formattedResp string
+
+	req := openai.ChatCompletionRequest{
+		Model:     openai.GPT3Dot5Turbo,
+		MaxTokens: 1000,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: prompt,
+			},
+		},
+		Stream: true,
+	}
+	stream, err := a.OpenAiClient.CreateChatCompletionStream(a.Context, req)
+	if err != nil {
+		return "exited with error while creating stream", err
+	}
+	defer stream.Close()
+
+	for {
+		response, err := stream.Recv()
+		// when response is nil, it means the stream is closed
+		// we then join the text segment chatgpt provides and return it as string
+		if errors.Is(err, io.EOF) {
+			formattedResp = strings.Join(buildResp, "")
+			return formattedResp, nil
+		}
+
+		if err != nil {
+			return "exited with error", err
+		}
+
+		// fmt.Printf(response.Choices[0].Delta.Content)
+
+		buildResp = append(buildResp, fmt.Sprintf(response.Choices[0].Delta.Content))
+	}
+}
