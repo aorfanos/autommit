9fa9a2d10f705625517f1714b3caeb3e3ee3e439
Alex Orfanos <aorfanos@users.noreply.github.com>
2023-04-23 03:29:58 +0300

    refactor: modify git.go
    Refactored the code in git.go by adding new variables for selectors, modifying the GitCommit function to prompt the user to proceed with the commit or not, and refactored the GitPush function to include selector prompt for pushing to remote or not. Created by autommit 🦄.

--- utils/git.go
+++ utils/git.go
@@ -1,7 +1,18 @@
 package utils
 
 import (
+	"fmt"
+	"os"
 	"os/exec"
+)
+
+var (
+	gitCommitSelectorQTitle = "Proceed with the commit?"
+	gitCommitSelectorQChoices = []string{"✅ Yes", "❌ No", "♻️ Regenerate"}
+	gitPushSelectorQTitle = "Push to remote?"
+	gitPushSelectorQChoices = []string{"✅ Yes", "❌ No"}
+
+    cmd *exec.Cmd
 )
 
 type Commit struct {
@@ -22,21 +33,44 @@ func GitAdd(path string) {
 }
 
 func GitDiff() (string) {
-	cmd := exec.Command("git", "diff", "-m", "HEAD")
+	cmd := exec.Command("git", "diff", "--staged", "HEAD")
 	diff, err := cmd.Output()
 	ErrCheck(err)
 	return string(diff)
 }
 
-func (a *Autommit) GitCommit(signCommits bool) (string) {
-	var cmd *exec.Cmd
-	if (signCommits) {
-		cmd = exec.Command("git", "commit", "-S", "-m", a.CommitInfo.Message, "-m", a.CommitInfo.MessageLong)
-	} else {
-		cmd = exec.Command("git", "commit", "-m", a.CommitInfo.Message, "-m", a.CommitInfo.MessageLong)
-	}
-	commit, err := cmd.Output()
+func (a *Autommit) GitCommit(signCommits bool) (regenerate bool) {
+	result, err := ProceedSelector(gitCommitSelectorQTitle, gitCommitSelectorQChoices)
 	ErrCheck(err)
-	return string(commit)
+	if (result == gitCommitSelectorQChoices[1]) { // no
+		fmt.Println("Commit aborted")
+		os.Exit(0)
+	} else if (result == gitCommitSelectorQChoices[2]) { // regenerate
+		return false
+	} else if (result == gitCommitSelectorQChoices[0]) { // yes
+		if (signCommits) {
+			cmd = exec.Command("git", "commit", "-S", "-m", a.CommitInfo.Message, "-m", a.CommitInfo.MessageLong)
+		} else {
+			cmd = exec.Command("git", "commit", "-m", a.CommitInfo.Message, "-m", a.CommitInfo.MessageLong)
+		}
+		_, err := cmd.Output()
+		ErrCheck(err)
+		return true
+	}
+	return
 }
- 
+
+func GitPush() error {
+	result, err := ProceedSelector(gitPushSelectorQTitle, gitPushSelectorQChoices)
+	ErrCheck(err)
+
+	if (result == gitPushSelectorQChoices[1]) { // no
+		fmt.Println("Push aborted")
+		return nil
+	} else if (result == gitPushSelectorQChoices[0]) { // yes
+		cmd := exec.Command("git", "push")
+		_, err := cmd.Output()
+		return err
+	}
+	return nil
+}
