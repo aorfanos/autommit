fda007fd2cedb82122d5ca874e7b1eaa870778f2
Alex Orfanos <aorfanos@users.noreply.github.com>
2023-04-23 20:36:54 +0000

    feat(autommit): add PgpSign boolean to Autommit struct
    feat(autommit): add PgpSign boolean to Autommit struct
    
    This commit adds a new boolean field to the Autommit struct called PgpSign. It will be used to determine whether or not to sign the commit with PGP.
    
    Created by autommit 🦄.

--- utils/autommit.go
+++ utils/autommit.go
@@ -11,6 +11,7 @@ type Autommit struct {
 	OpenAiApiKey string
 	Context context.Context
 	OpenAiClient openai.Client
+	PgpSign bool
 	CommitInfo Commit
 }
 
@@ -24,6 +25,11 @@ func NewAutommit(openAiApiKey string) (*Autommit) {
 	}
 }
 
+
+// function GeneratePrompt generates a prompt for the OpenAI API
+// @param gitDiff string
+// @param footer string
+// @return string
 func (a *Autommit) GeneratePrompt(gitDiff string, footer string) (string) {
 	var prologue = "Analyze the following output of git diff and create a git commit message in json format, describing the changes following the Conventional Commits specification (study https://www.conventionalcommits.org/en/v1.0.0/#specification). Json fields are only: commit_message, commit_message_long."
 	var restrictions = "Include only truthful information relevant to the git diff output. If there are multiple files, provide a detailed changelog in commit_message_long. commit_message should always be short and concise. Do not return anything else other than JSON response."
