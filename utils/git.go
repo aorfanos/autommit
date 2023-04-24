331b003c9f7769c2677dea829df7cb492b6ba90a
Alex Orfanos <aorfanos@users.noreply.github.com>
2023-04-25 01:19:35 +0300

    chore(utils): remove unused imports
    Remove unused imports from the git.go file in the utils directory. Created by autommit 🦄.

--- utils/git.go
+++ utils/git.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"time"
 
 	git "github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/manifoldco/promptui"
 )
 
@@ -139,11 +137,11 @@ func (a *Autommit) GitCommit() (error) {
 	commit, err := a.GitConfig.Worktree.Commit(
 		fmt.Sprintf("%s\n\n%s", a.CommitInfo.Message, a.CommitInfo.MessageLong),
 		&git.CommitOptions{
-			Author: &object.Signature{
-				Name: a.GitConfig.Author,
-				Email: a.GitConfig.AuthorMail,
-				When: time.Now(),
-			},
+			// Author: &object.Signature{
+			// 	Name: a.GitConfig.Author,
+			// 	Email: a.GitConfig.AuthorMail,
+			// 	When: time.Now(),
+			// },
 		})
 	ErrCheck(err)
 	_, err = a.GitConfig.Repo.CommitObject(commit)
